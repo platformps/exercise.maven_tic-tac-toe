1addbb08f697284365d7687f6e0f5dad9331e444
CJ Fulton <fulchr3356@gmail.com>
2020-07-14 17:21:20 -0400

    Completed exercise

--- src/main/java/com/github/perscholas/Board.java
+++ src/main/java/com/github/perscholas/Board.java
@@ -4,23 +4,55 @@ package com.github.perscholas;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    private final Character[][] board;
+
     public Board(Character[][] matrix) {
+        this.board = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        return isInFavorOf('X');
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+
+        return isInFavorOf('O');
     }
 
+
+
     public Boolean isTie() {
-        return null;
+
+        return !isInFavorOfO() &&!isInFavorOfX();
     }
 
     public String getWinner() {
-        return null;
+        if (isInFavorOfX())
+        return "X";
+        else if(isInFavorOfO())
+            return "O";
+        return "";
+    }
+
+    public boolean isInFavorOf(char XO){
+        if(board[1][0].equals(XO) && board[1][1].equals(XO) &&board[1][2].equals(XO))
+            return true;
+        else if(board[0][0].equals(XO) && board[0][1].equals(XO) &&board[0][2].equals(XO))
+            return true;
+        else if(board[2][0].equals(XO) && board[2][1].equals(XO) &&board[2][2].equals(XO))
+            return true;
+        else if(board[0][0].equals(XO) && board[1][0].equals(XO) &&board[2][0].equals(XO))
+            return true;
+        else if(board[0][1].equals(XO) && board[1][1].equals(XO) &&board[2][1].equals(XO))
+            return true;
+        else if(board[0][2].equals(XO) && board[1][2].equals(XO) &&board[2][2].equals(XO))
+            return true;
+        else if(board[0][0].equals(XO) && board[1][1].equals(XO) &&board[2][2].equals(XO))
+            return true;
+        else  if(board[0][2].equals(XO) && board[1][1].equals(XO) &&board[2][0].equals(XO))
+            return true;
+        return false;
     }
 
 }
