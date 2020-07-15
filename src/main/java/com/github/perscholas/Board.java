dc27673dca556f97e77499fa5e95d79750352d2a
bwcsemaj <bwc_semaj@live.com>
2020-07-15 13:49:47 -0400

    Finished exercise hardcoded for 3x3

--- src/main/java/com/github/perscholas/Board.java
+++ src/main/java/com/github/perscholas/Board.java
@@ -1,26 +1,71 @@
 package com.github.perscholas;
 
+import java.time.Instant;
+import java.util.Arrays;
+
 /**
  * @author leon on 6/22/18.
  */
 public class Board {
+    
+    //Attributes
+    private final Character[][] matrix;
+    
+    private static final int[][][] WIN_CONDITIONS = {
+            {{0, 0}, {0, 1}, {0, 2}},
+            {{1, 0}, {1, 1}, {1, 2}},
+            {{2, 0}, {2, 1}, {2, 2}},
+            {{0, 0}, {1, 0}, {2, 0}},
+            {{0, 1}, {1, 1}, {2, 1}},
+            {{0, 2}, {1, 2}, {2, 2}},
+            {{0, 0}, {1, 1}, {2, 2}},
+            {{2, 0}, {1, 1}, {0, 2}}
+    };
+    
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
-
+    
     public Boolean isInFavorOfX() {
-        return null;
+        return isWin('X');
     }
-
+    
     public Boolean isInFavorOfO() {
-        return null;
+        return isWin('O');
     }
-
+    
     public Boolean isTie() {
-        return null;
+        return !isInFavorOfO() && !isInFavorOfX();
     }
-
+    
     public String getWinner() {
-        return null;
+        if(isInFavorOfO()){
+            return "O";
+        } else if(isInFavorOfX()){
+            return "X";
+        }
+        return "";
     }
+    
+    private boolean isWin(char character) {
+        for(int index = 0; WIN_CONDITIONS.length > index; index++){
+            int[][] winConditionPoints = WIN_CONDITIONS[index];
+            if(isWin(winConditionPoints, character)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
 
+    
+    private boolean isWin(int[][] winConditionPoints, char character){
+        for(int index = 0; winConditionPoints.length >index; index++){
+            int[] point = winConditionPoints[index];
+            if(matrix[point[0]][point[1]] != character){
+                return false;
+            }
+        }
+        return true;
+    }
 }
