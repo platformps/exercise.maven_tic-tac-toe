aab00e2d9f3273bd05c4064433a3b6140932fd1d
nick-devos <nickdevos123@yahoo.com>
2020-07-15 17:30:29 -0400

    finished

--- src/main/java/com/github/perscholas/Board.java
+++ src/main/java/com/github/perscholas/Board.java
@@ -37,28 +37,28 @@ public class Board {
     }
 
     public String determineWinner() {
-        if(board[0][0] == board[0][1] && board[0][1] == board[0][2]) { //row 0
+        if(board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] != ' ') { //row 0
             return (board[0][0]).toString(); //"X" or "O"
         }
-        else if(board[1][0] == board[1][1] && board[1][1] == board[1][2]) { //row 1
+        else if(board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] != ' ') { //row 1
             return (board[1][0]).toString(); //"X" or "O"
         }
-        else if(board[2][0] == board[2][1] && board[2][1] == board[2][2]) { //row 2
+        else if(board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] != ' ') { //row 2
             return (board[2][0]).toString(); //"X" or "O"
         }
-        else if(board[0][0] == board[1][0] && board[1][0] == board[2][0]) { //column 0
+        else if(board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[0][0] != ' ') { //column 0
             return (board[0][0]).toString(); //"X" or "O"
         }
-        else if(board[0][1] == board[1][1] && board[1][1] == board[2][1]) { //column 1
+        else if(board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[0][1] != ' ') { //column 1
             return (board[0][1]).toString(); //"X" or "O"
         }
-        else if(board[0][2] == board[1][2] && board[1][2] == board[2][2]) { //column 2
+        else if(board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] != ' ') { //column 2
             return (board[0][2]).toString(); //"X" or "O"
         }
-        else if(board[0][0] == board[1][1] && board[1][1] == board[2][2]) { //top-left -> bottom-right diagonal
+        else if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') { //top-left -> bottom-right diagonal
             return (board[0][0]).toString(); //"X" or "O"
         }
-        else if(board[0][2] == board[1][1] && board[1][1] == board[2][0]) { //top-right -> bottom-left diagonal
+        else if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') { //top-right -> bottom-left diagonal
             return (board[0][2]).toString(); //"X" or "O"
         }
         else {
