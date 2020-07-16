01674862b892f73e5630ee88bbf9e3fd5e87b4a2
razeroual <47251928+razeroual@users.noreply.github.com>
2020-07-16 13:31:37 -0400

    make the code more clear

--- src/main/java/com/github/perscholas/Board.java
+++ src/main/java/com/github/perscholas/Board.java
@@ -10,89 +10,74 @@ public class Board {
     }
 
     public Boolean isInFavorOfX() {
-        if(isPlayerXWins())
+        if(isPlayerWins('X'))
             return true;
         else
             return false;
     }
 
     public Boolean isInFavorOfO() {
-        if(isPlayerOWins())
+        if(isPlayerWins('O'))
             return true;
         else
             return false;
     }
 
     public Boolean isTie() {
-        if(isPlayerOWins()== false && isPlayerXWins() == false)
+        if(isInFavorOfO()== false && isInFavorOfX() == false)
             return true;
         else
             return false;
     }
 
     public String getWinner() {
-        if(isPlayerXWins())
+        if(isInFavorOfX())
             return "X";
-        else if (isPlayerOWins())
+        else if (isInFavorOfO())
                 return "O";
             else
                 return "";
     }
 
 
-    public boolean isPlayerXWins(){
-        Boolean isMatchX = true;
-        for (int i = 0; i < 3; i++) {
-            String concatX ="";
-            for (int j = 0; j < 3; j++) {
-                concatX = concatX + boardMatrix[i][j];
-            }
-            if (("XXX").equals(concatX)){
-                return true;
-            }
-        }
-        for (int i = 0; i < 3; i++) {
-            Character toStartX = 'X';
-            String concatX ="";
-            for (int j = 0; j < 3; j++) {
-                concatX = concatX + boardMatrix[j][i];
-            }
-            if (("XXX").equals(concatX)){
-                return true;
-            }
-        }
-        if((boardMatrix[0][0]+""+boardMatrix[1][1]+""+boardMatrix[2][2]+"").equals("XXX"))
-            return true;
-        if((boardMatrix[0][2]+""+boardMatrix[1][1]+""+boardMatrix[2][0]+"").equals("XXX"))
-            return true;
 
-        return false;
-    }
-    public boolean isPlayerOWins(){
+    public boolean isPlayerWins(char player){
+
+        // concat character player to make it "XXX" or "OOO"
+        String numberOfligne = "";
+        for (int i = 0; i < 3; i++) {
+            numberOfligne = numberOfligne + player;
+        }
+
+        // check if any line match
         Boolean isMatchX = true;
         for (int i = 0; i < 3; i++) {
             String concatX ="";
             for (int j = 0; j < 3; j++) {
                 concatX = concatX + boardMatrix[i][j];
             }
-            if (concatX.equals("OOO")){
+            if (concatX.equals(numberOfligne)){
                 return true;
             }
         }
+
+        // check if any column match
         for (int i = 0; i < 3; i++) {
-            Character toStartX = 'O';
             String concatX ="";
             for (int j = 0; j < 3; j++) {
                 concatX = concatX + boardMatrix[j][i];
             }
-            if (concatX.equals("OOO")){
+            if (concatX.equals(numberOfligne)){
                 return true;
             }
         }
-        if((boardMatrix[0][0]+""+boardMatrix[1][1]+""+boardMatrix[2][2]+"").equals("OOO")){
+
+        // check \
+        if((boardMatrix[0][0]+""+boardMatrix[1][1]+""+boardMatrix[2][2]+"").equals(numberOfligne)){
             return true;
         }
-        if((boardMatrix[0][2]+""+boardMatrix[1][1]+""+boardMatrix[2][0]+"").equals("OOO"))
+        //check /
+        if((boardMatrix[0][2]+""+boardMatrix[1][1]+""+boardMatrix[2][0]+"").equals(numberOfligne))
             return true;
 
         return false;
