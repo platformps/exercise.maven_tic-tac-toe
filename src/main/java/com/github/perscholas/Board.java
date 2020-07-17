12f55c94c8db0ac45167b0c4d1ed3b7f1b664f8c
David Frary <fraryd@gmail.com>
2020-07-16 20:32:31 -0400

    finished edits on 'Board.java'

--- src/main/java/com/github/perscholas/Board.java
+++ src/main/java/com/github/perscholas/Board.java
@@ -4,14 +4,16 @@ package com.github.perscholas;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private final Character[][] board;
     public Board(Character[][] matrix) {
+        this.board = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return true;
+        return Score('X');
     }
     public Boolean isInFavorOfO() {
-        return false;
+        return Score('O');
     }
 
     public Boolean isTie() {
@@ -25,6 +27,30 @@ public class Board {
             return "X";
         else
             return "";
+    }
+
+    public boolean Score(char XO){
+        if(board[1][0].equals(XO) && board[1][1].equals(XO) &&board[1][2].equals(XO))
+            return true;
+        else if(board[0][0].equals(XO) && board[0][1].equals(XO) &&board[0][2].equals(XO))
+            return true;
+        else if(board[0][0].equals(XO) && board[1][0].equals(XO) &&board[2][0].equals(XO))
+            return true;
+        else if(board[2][0].equals(XO) && board[2][1].equals(XO) &&board[2][2].equals(XO))
+            return true;
+        else if(board[0][2].equals(XO) && board[1][2].equals(XO) &&board[2][2].equals(XO))
+            return true;
+        else if(board[0][2].equals(XO) && board[1][1].equals(XO) &&board[2][0].equals(XO))
+            return true;
+        else if(board[0][1].equals(XO) && board[1][1].equals(XO) &&board[2][1].equals(XO))
+            return true;
+        else if(board[0][0].equals(XO) && board[1][1].equals(XO) &&board[2][2].equals(XO))
+
+            return true;
+
+        else
+
+            return false;
     }
 
 
