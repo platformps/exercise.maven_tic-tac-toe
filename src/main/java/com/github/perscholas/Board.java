addaba3e98542cc375022a0e49631b40b7363736
ANIL CHADDHA <a101981c@ANILs-MacBook-Air.local>
2020-07-23 19:52:26 -0400

    Tic Tac Toe completed

--- src/main/java/com/github/perscholas/Board.java
+++ src/main/java/com/github/perscholas/Board.java
@@ -4,23 +4,56 @@ package com.github.perscholas;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private final Character[][] board;
     public Board(Character[][] matrix) {
+        this.board = matrix;
+
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        return isInFavorOf('X');
     }
 
-    public Boolean isInFavorOfO() {
-        return null;
+    public Boolean isInFavorOfO()
+    {
+        return isInFavorOf('O');
     }
 
     public Boolean isTie() {
-        return null;
+
+        return !isInFavorOfO() &&!isInFavorOfX();
     }
 
-    public String getWinner() {
-        return null;
+    public String getWinner()
+    {
+        if(isInFavorOfX())
+            return "X";
+        else if(isInFavorOfO())
+            return "O";
+        return "";
+    }
+
+    public boolean isInFavorOf(char XO){
+        //checking all row values
+        if (board[0][0].equals(XO) && board[0][1].equals(XO) && board[0][2].equals(XO))
+            return true;
+        else if(board[1][0].equals(XO) && board[1][1].equals(XO) && board[1][2].equals(XO))
+            return true;
+        else if (board[2][0].equals(XO) && board[2][1].equals(XO) && board[2][2].equals(XO))
+            return true;
+        //checking all column values
+        else if (board[0][0].equals(XO) && board[1][0].equals(XO) && board[2][0].equals(XO))
+            return true;
+        else if (board[0][1].equals(XO) && board[1][1].equals(XO) && board[2][1].equals(XO))
+            return true;
+        else if (board[0][2].equals(XO) && board[1][2].equals(XO) && board[2][2].equals(XO))
+            return true;
+        //checking all diagonal values
+        else if (board[0][0].equals(XO) && board[1][1].equals(XO) && board[2][2].equals(XO))
+            return true;
+        else if (board[0][2].equals(XO) && board[1][1].equals(XO) && board[2][0].equals(XO))
+            return true;
+        return false;
     }
 
 }
